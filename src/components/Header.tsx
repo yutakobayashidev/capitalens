591c1b3280ba136a2c52ba911ee758deff6ad302
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-03-19 10:38:51 +0900

    ✨ add beta header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,17 @@ export default function Header() {
     <header className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="flex h-20 items-center justify-between">
-          <Link href="/" className="font-bold font-base text-3xl">
+          <Link
+            href="/"
+            className="font-bold font-base text-2xl md:text-3xl flex items-center"
+          >
             国会発言分析
+            <span className="text-sm ml-3 text-gray-400 border py-0.5 px-1 font-medium">
+              BETA
+            </span>
           </Link>
           <div>
-            <Link href="/about" className="font-bold text-lg">
+            <Link href="/about" className="font-bold text-base md:text-lg">
               About
             </Link>
           </div>
