88aa3b61a0a97787966f9f8ac15a8ebe49d69441
yutakobayashidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-03-23 09:44:47 +0900

    ✨ feat: data source section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -21,6 +21,17 @@ export default async function Page() {
           <p>
             当サイトは、公正かつ客観的な情報提供を目指し、市民やメディア、政治家など様々な人々が、正確かつ透明な情報を得ることができる場として、貢献することを目指しています。
           </p>
+          <h2>データ</h2>
+          <p>
+            当サイトは国立国会図書館が運営する
+            <a
+              className="text-[#0f41af] hover:underline hover:text-[#222]"
+              href="https://kokkai.ndl.go.jp/api.html"
+            >
+              国会会議録検索システム　検索用API
+            </a>
+            からデータを参照している場合があります。また、当サイトで使用されているアルゴリズムはオープンソースで公開されています。
+          </p>
         </div>
       </section>
     </div>
