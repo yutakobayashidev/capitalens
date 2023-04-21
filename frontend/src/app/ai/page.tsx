61e22a41b2e4076b576e03152a6ebc1a88b0f086
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-04-22 03:39:35 +0900

    ✨ add people info

--- frontend/src/app/ai/page.tsx
+++ frontend/src/app/ai/page.tsx
@@ -1,28 +1,62 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FaceDetection from '@src/app/ai/FaceDetection';
+import { getPeopleByName } from '@src/helper/people';
+import { People } from '@src/types/people';
+import Link from 'next/link';
 
 const IndexPage = () => {
-  const [name, setName] = useState<string | null>(null);
+  const [name, setName] = useState<People | null>(null);
+
+  const [error, setError] = useState(false);
 
   const handleFaceDetect = (name: string) => {
-    setName(name);
+    if (name !== 'unknown') {
+      const people = getPeopleByName(name);
+      if (people) {
+        setName(people);
+      } else {
+        setError(true);
+      }
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <section>
       <div>
         <FaceDetection onFaceDetect={handleFaceDetect} />
-        {name !== null && (
-          <div className='mt-10 text-center'>
-            {name === 'unknown' ? (
-              <p className='text-4xl font-bold'>見つかりませんでした</p>
-            ) : (
-              <p className='text-4xl font-bold'>{name}</p>
-            )}
-          </div>
-        )}
+        <div className='mt-10'>
+          {error === true ? (
+            <p className='text-4xl font-bold'>見つかりませんでした</p>
+          ) : (
+            <>
+              {name && name.name && (
+                <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
+                  <div className='flex items-center'>
+                    <img
+                      className='rounded-full mr-3'
+                      height={60}
+                      width={60}
+                      src={name.image}
+                      alt={name.name}
+                    />
+                    <h1 className='text-2xl font-bold'>{name.name}</h1>
+                  </div>
+                  <p className='my-3'>{name.bio}</p>
+                  <Link
+                    className='mb-5 block text-[#0f41af]'
+                    href={`/people/${name.id}`}
+                  >
+                    情報を詳しく見る -&gt;
+                  </Link>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       </div>
     </section>
   );
