07f5dd8358ab461d3172bed7f70c160ed8205068
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-05-14 07:01:03 +0900

    🐞 remove server actions

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     disableStaticImages: true,
   },
