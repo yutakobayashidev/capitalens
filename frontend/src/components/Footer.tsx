307a245ba6626c102ccca1b77b305369657c3389
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-05-29 15:42:07 +0900

    💄 new footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,26 +2,86 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-white py-4 text-center border-t">
-      <div className="mx-auto max-w-screen-xl px-4 md:px-8">
-        <div className="text-gray-400">© CapitaLens</div>
-        <nav className="gap-x-4 inline-flex mt-4">
-          <a
-            href="https://forms.gle/d3n196yyQ6V4GdEt6"
-            className="hover:underline text-gray-500"
-          >
-            お問い合わせ
-          </a>
-          <Link className="hover:underline text-gray-500" href="/data">
-            使用データ
-          </Link>
-          <Link className="hover:underline text-gray-500" href="/guideline">
-            ガイドライン
-          </Link>
-          <Link className="hover:underline text-gray-500" href="/privacy">
-            プライバシーポリシー
-          </Link>
-        </nav>
+    <footer className="bg-white pt-10 pb-8">
+      <div className="mx-auto max-w-[1200px] px-4 md:px-8">
+        <div className="md:grid-cols-4 grid gap-10">
+          <div>
+            <Link className="mb-3 block text-2xl font-bold" href="/">
+              CapitaLens
+            </Link>
+            <p className="text-gray-500 text-sm">国のデータを整理する</p>
+          </div>
+          <nav>
+            <h4 className="font-bold">プロジェクトについて</h4>
+            <ul>
+              <li className="my-3">
+                <Link className="text-sm hover:underline" href="/about">
+                  このサイトについて
+                </Link>
+              </li>
+              <li className="my-3">
+                <a
+                  className="text-sm hover:underline"
+                  href="https://github.com/users/yutakobayashidev/projects/2"
+                >
+                  開発ロードマップ
+                </a>
+              </li>
+              <li className="my-3">
+                <a
+                  className="text-sm hover:underline"
+                  href="https://forms.gle/d3n196yyQ6V4GdEt6"
+                >
+                  お問い合わせ
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <h4 className="font-bold">リンク & ソーシャル</h4>
+            <ul>
+              <li className="my-3">
+                <a
+                  className="text-sm hover:underline"
+                  href="https://github.com/yutakobayashidev/capitalens"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li className="my-3">
+                <a
+                  className="text-sm hover:underline"
+                  href="https://discord.gg/tcc5AvRSr9"
+                >
+                  Discord
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <h4 className="font-bold">法的</h4>
+            <ul>
+              <li className="my-3">
+                <Link className="text-sm hover:underline" href="/data">
+                  使用データ
+                </Link>
+              </li>
+              <li className="my-3">
+                <Link className="text-sm hover:underline" href="/privacy">
+                  プライバシーポリシー
+                </Link>
+              </li>
+              <li className="my-3">
+                <Link className="text-sm hover:underline" href="/guideline">
+                  ガイドライン
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="text-gray-400 text-center pt-4 mt-5 border-t">
+          © CapitaLens
+        </div>
       </div>
     </footer>
   );
