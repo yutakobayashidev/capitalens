9214aece2db5b70b8ec2f1cf66c6fe4f17beea55
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-05-30 23:38:03 +0900

    ⬆️ install dependencies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   images: {
     disableStaticImages: true,
   },
@@ -17,6 +22,6 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-};
+});
 
 module.exports = withContentlayer(nextConfig);
