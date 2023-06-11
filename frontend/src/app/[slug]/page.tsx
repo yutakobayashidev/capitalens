40f3806bd894ecf37e2276ebf6dbed175aa07154
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-06-11 21:47:48 +0900

    ✨ chore: add github link

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { allPages } from "contentlayer/generated";
 import { notFound } from "next/navigation";
 import { config } from "@site.config";
+import { FaGithub } from "react-icons/fa";
 
 export async function generateMetadata({
   params,
@@ -52,6 +53,21 @@ export default async function Page({ params }: { params: { slug: string } }) {
     <div className="mx-auto max-w-screen-sm px-4 md:px-8 my-12">
       <section>
         <h1 className="font-bold mb-7 text-4xl text-center">{page.title}</h1>
+        <div className="flex items-center mb-5 p-4 text-gray-500 bg-gray-100 rounded-lg">
+          <FaGithub className="text-xl mr-2" />
+          {page.title}の変更履歴は
+          <a
+            className="underline text-gray-600"
+            href={
+              config.SocialLinks.github +
+              "/commits/main/frontend/content/page/" +
+              page._raw.sourceFileName
+            }
+          >
+            GitHub
+          </a>
+          で確認できます
+        </div>
         <div
           className="prose prose-a:no-underline hover:prose-a:underline prose-a:text-primary prose-neutral max-w-none"
           dangerouslySetInnerHTML={{ __html: page.body.html }}
