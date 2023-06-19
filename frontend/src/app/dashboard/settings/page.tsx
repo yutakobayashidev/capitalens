451bac196c2be7a8141261e5a29744fa80054e2d
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-06-19 16:26:53 +0900

    ✨ chore: add metadata

--- frontend/src/app/dashboard/settings/page.tsx
+++ frontend/src/app/dashboard/settings/page.tsx
@@ -1,5 +1,10 @@
 import Settings from "./Settings";
 import prisma from "@src/lib/prisma";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "アカウント設定",
+};
 
 export default async function Page() {
   const prefectures = await prisma.prefecture.findMany();
