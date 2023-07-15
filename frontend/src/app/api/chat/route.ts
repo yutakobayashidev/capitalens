45f55424bfbf76070ce7893086d7330dd7a27348
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-07-15 16:39:35 +0900

    ♻️ refactor

--- frontend/src/app/api/chat/route.ts
+++ frontend/src/app/api/chat/route.ts
@@ -1,4 +1,3 @@
-import { connect } from "@planetscale/database";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { OpenAIStream, StreamingTextResponse } from "ai";
@@ -6,22 +5,15 @@ import { Configuration, OpenAIApi } from "openai-edge";
 
 import { functions, runFunction } from "./functions";
 
-const pscale_config = {
-  url: process.env.DATABASE_URL || "mysql://user:pass@host",
-};
-
-const conn = process.env.DATABASE_URL ? connect(pscale_config) : null;
-
 export const runtime = "edge";
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const apiConfig = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY!,
 });
-const openai = new OpenAIApi(config);
+
+const openai = new OpenAIApi(apiConfig);
 
 export async function POST(req: Request) {
-  if (!conn) return null;
-
   if (
     process.env.NODE_ENV != "development" &&
     process.env.UPSTASH_REDIS_REST_URL &&
@@ -55,58 +47,30 @@ export async function POST(req: Request) {
 
   const { messages } = await req.json();
 
-  const initialResponse = await openai.createChatCompletion({
-    function_call: "auto",
+  const response = await openai.createChatCompletion({
     functions,
     messages,
     model: "gpt-3.5-turbo-0613",
+    stream: true,
+    temperature: 0,
   });
 
-  const initialResponseJson = await initialResponse.json();
-  const initialResponseMessage = initialResponseJson?.choices?.[0]?.message;
+  const stream = OpenAIStream(response, {
+    experimental_onFunctionCall: async (
+      { name, arguments: args },
+      createFunctionCallMessages
+    ) => {
+      const functionResponse = await runFunction(name, args);
 
-  console.log(initialResponseJson);
+      const newMessages = createFunctionCallMessages(functionResponse);
+      return openai.createChatCompletion({
+        functions,
+        messages: [...messages, ...newMessages],
+        model: "gpt-3.5-turbo-0613",
+        stream: true,
+      });
+    },
+  });
 
-  let finalResponse;
-
-  if (initialResponseMessage.function_call) {
-    const { name, arguments: args } = initialResponseMessage.function_call;
-    const functionResponse = await runFunction(name, JSON.parse(args));
-
-    finalResponse = await openai.createChatCompletion({
-      messages: [
-        ...messages,
-        initialResponseMessage,
-        {
-          name: initialResponseMessage.function_call.name,
-          content: JSON.stringify(functionResponse),
-          role: "function",
-        },
-      ],
-      model: "gpt-3.5-turbo-0613",
-      stream: true,
-    });
-
-    const stream = OpenAIStream(finalResponse);
-    return new StreamingTextResponse(stream);
-  } else {
-    const chunks = initialResponseMessage.content.split(" ");
-    const stream = new ReadableStream({
-      async start(controller) {
-        for (const chunk of chunks) {
-          const bytes = new TextEncoder().encode(chunk + " ");
-          controller.enqueue(bytes);
-          await new Promise((r) =>
-            setTimeout(
-              r,
-              // get a random number between 10ms and 30ms to simulate a random delay
-              Math.floor(Math.random() * 20 + 10)
-            )
-          );
-        }
-        controller.close();
-      },
-    });
-    return new StreamingTextResponse(stream);
-  }
+  return new StreamingTextResponse(stream);
 }
