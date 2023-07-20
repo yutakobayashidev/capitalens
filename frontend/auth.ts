6141f5bb0a17af98a0f01c597642979dbaf6fbf4
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-07-20 19:36:50 +0900

    🐞 fix type

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,29 +1,31 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@src/lib/prisma";
 import NextAuth, { type DefaultSession } from "next-auth";
 import Google from "next-auth/providers/google";
-import prisma from "@src/lib/prisma";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      createdAt: string;
       kids: boolean;
       prefectureId: null | number;
+      updatedAt: string;
     } & DefaultSession["user"];
   }
 }
 
 export const {
-  handlers: { GET, POST },
   auth,
   CSRF_experimental,
+  handlers: { GET, POST },
 } = NextAuth({
-  secret: process.env.SECRET,
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
 });
